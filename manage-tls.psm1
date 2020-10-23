1dab47c8d80a9bae459fd5b6b2f48d8c0cb47c24
Rex Linder <rex@kuan51.com>
2020-10-22 23:18:48 -0600

    Update manage-tls.psm1
    Simplified paths with base_path variable.  Added error handling and colors for better UX.

--- manage-tls.psm1
+++ manage-tls.psm1
@@ -14,128 +14,179 @@ function Manage-Tls
          [bool] $tls12
     )
 
+    $base_path = 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols'
+    $fg_color = 'CYAN'
+
     switch($PsCmdlet.ParameterSetName){
     "ssl2" { ssl2 }
     "ssl3" { ssl3 }
     "tls1" { tls1 }
-    "tl11" { tl11 }
+    "tls11" { tls11 }
     "tls12" { tls12 }
     }
 }
 
 function ssl2 {
+  try {
     if($ssl2){
-        New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Server' -Force | Out-Null
-        New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Server' -name 'Enabled' -value '1' -PropertyType 'DWord' -Force | Out-Null
-        New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Server' -name 'DisabledByDefault' -value 0 -PropertyType 'DWord' -Force | Out-Null
-        New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Client' -Force | Out-Null
-        New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Client' -name 'Enabled' -value '1' -PropertyType 'DWord' -Force | Out-Null
-        New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Client' -name 'DisabledByDefault' -value 0 -PropertyType 'DWord' -Force | Out-Null
-        Write-Host "SSLv2 Enabled"
+      New-Item "$base_path\SSL 2.0\Server" -Force | Out-Null
+      New-ItemProperty -path "$base_path\SSL 2.0\Server" -name 'Enabled' -value '1' -PropertyType 'DWord' -Force | Out-Null
+      New-ItemProperty -path "$base_path\SSL 2.0\Server" -name 'DisabledByDefault' -value 0 -PropertyType 'DWord' -Force | Out-Null
+      New-Item "$base_path\SSL 2.0\Client" -Force | Out-Null
+      New-ItemProperty -path "$base_path\SSL 2.0\Client" -name 'Enabled' -value '1' -PropertyType 'DWord' -Force | Out-Null
+      New-ItemProperty -path "$base_path\SSL 2.0\Client" -name 'DisabledByDefault' -value 0 -PropertyType 'DWord' -Force | Out-Null
+      Write-Host "SSLv2 Enabled" -ForegroundColor $fg_color
     }
-    else{
-        New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Server' -Force | Out-Null
-        New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Server' -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
-        New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Server' -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force | Out-Null
-        New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Client' -Force | Out-Null
-        New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Client' -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
-        New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Client' -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force | Out-Null
-        Write-Host "SSLv2 Disabled"
+    else {
+      New-Item "$base_path\SSL 2.0\Server" -Force | Out-Null
+      New-ItemProperty -path "$base_path\SSL 2.0\Server" -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
+      New-ItemProperty -path "$base_path\SSL 2.0\Server" -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force | Out-Null
+      New-Item "$base_path\SSL 2.0\Client" -Force | Out-Null
+      New-ItemProperty -path "$base_path\SSL 2.0\Client" -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
+      New-ItemProperty -path "$base_path\SSL 2.0\Client" -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force | Out-Null
+      Write-Host "SSLv2 Disabled" -ForegroundColor $fg_color
     }
+  }
+  catch {
+    Write-Host $_.Exception.Message -ForegroundColor RED
+  }
+  finally {
+    $Error.Clear()
+  }
 }
 
 function ssl3 {
+  try {
     if($ssl3){
-        New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Server' -Force | Out-Null
-        New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Server' -name 'Enabled' -value '1' -PropertyType 'DWord' -Force | Out-Null
-        New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Server' -name 'DisabledByDefault' -value 0 -PropertyType 'DWord' -Force | Out-Null
-        New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Client' -Force | Out-Null
-        New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Client' -name 'Enabled' -value '1' -PropertyType 'DWord' -Force | Out-Null
-        New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Client' -name 'DisabledByDefault' -value 0 -PropertyType 'DWord' -Force | Out-Null
-        Write-Host "SSLv2 Enabled"
+      New-Item "$base_path\SSL 3.0\Server" -Force | Out-Null
+      New-ItemProperty -path "$base_path\SSL 3.0\Server" -name 'Enabled' -value '1' -PropertyType 'DWord' -Force | Out-Null
+      New-ItemProperty -path "$base_path\SSL 3.0\Server" -name 'DisabledByDefault' -value 0 -PropertyType 'DWord' -Force | Out-Null
+      New-Item "$base_path\SSL 3.0\Client" -Force | Out-Null
+      New-ItemProperty -path "$base_path\SSL 3.0\Client" -name 'Enabled' -value '1' -PropertyType 'DWord' -Force | Out-Null
+      New-ItemProperty -path "$base_path\SSL 3.0\Client" -name 'DisabledByDefault' -value 0 -PropertyType 'DWord' -Force | Out-Null
+      Write-Host "SSLv3 Enabled" -ForegroundColor $fg_color
     }
-    else{
-        New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Server' -Force | Out-Null
-        New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Server' -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
-        New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Server' -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force | Out-Null
-        New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 2.0\Client' -Force | Out-Null
-        New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Client' -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
-        New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Client' -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force | Out-Null
-        Write-Host "SSLv2 Disabled"
+    else {
+      New-Item "$base_path\SSL 3.0\Server" -Force | Out-Null
+      New-ItemProperty -path "$base_path\SSL 3.0\Server" -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
+      New-ItemProperty -path "$base_path\SSL 3.0\Server" -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force | Out-Null
+      New-Item "$base_path\SSL 3.0\Client" -Force | Out-Null
+      New-ItemProperty -path "$base_path\SSL 3.0\Client" -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
+      New-ItemProperty -path "$base_path\SSL 3.0\Client" -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force | Out-Null
+      Write-Host "SSLv3 Disabled" -ForegroundColor $fg_color
     }
+  }
+  catch {
+    Write-Host $_.Exception.Message -ForegroundColor RED
+  }
+  finally {
+    $Error.Clear()
+  }
 }
 
 function tls1 {
+  try {
     if($tls1){
-        New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server' -Force | Out-Null
-	New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server' -name 'Enabled' -value '1' -PropertyType 'DWord' -Force | Out-Null
-	New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server' -name 'DisabledByDefault' -value 0 -PropertyType 'DWord' -Force | Out-Null
-	New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client' -Force | Out-Null
-	New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client' -name 'Enabled' -value '1' -PropertyType 'DWord' -Force | Out-Null
-	New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client' -name 'DisabledByDefault' -value 0 -PropertyType 'DWord' -Force | Out-Null
-        Write-Host "TLSv1 Enabled"
+      New-Item "$base_path\TLS 1.0\Server" -Force | Out-Null
+      New-ItemProperty -path "$base_path\TLS 1.0\Server" -name 'Enabled' -value '1' -PropertyType 'DWord' -Force | Out-Null
+      New-ItemProperty -path "$base_path\TLS 1.0\Server" -name 'DisabledByDefault' -value 0 -PropertyType 'DWord' -Force | Out-Null
+      New-Item "$base_path\TLS 1.0\Client" -Force | Out-Null
+      New-ItemProperty -path "$base_path\TLS 1.0\Client" -name 'Enabled' -value '1' -PropertyType 'DWord' -Force | Out-Null
+      New-ItemProperty -path "$base_path\TLS 1.0\Client" -name 'DisabledByDefault' -value 0 -PropertyType 'DWord' -Force | Out-Null
+      Write-Host "TLSv1 Enabled" -ForegroundColor $fg_color
     }
-    else{
-        New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server' -Force | Out-Null
-	New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server' -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
-	New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Server' -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force | Out-Null
-	New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client' -Force | Out-Null
-	New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client' -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
-	New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.0\Client' -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force | Out-Null
-        Write-Host "TLSv1 Disabled"
+    else {
+      New-Item "$base_path\TLS 1.0\Server" -Force | Out-Null
+      New-ItemProperty -path "$base_path\TLS 1.0\Server" -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
+      New-ItemProperty -path "$base_path\TLS 1.0\Server" -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force | Out-Null
+      New-Item "$base_path\TLS 1.0\Client" -Force | Out-Null
+      New-ItemProperty -path "$base_path\TLS 1.0\Client" -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
+      New-ItemProperty -path "$base_path\TLS 1.0\Client" -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force | Out-Null
+      Write-Host "TLSv1 Disabled" -ForegroundColor $fg_color
     }
+  }
+  catch {
+    Write-Host $_.Exception.Message -ForegroundColor RED
+  }
+  finally {
+    $Error.Clear()
+  }
 }
 
 function tls11 {
+  try {
     if($tls11){
-        New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server' -Force | Out-Null
-	New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server' -name 'Enabled' -value '1' -PropertyType 'DWord' -Force | Out-Null
-	New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server' -name 'DisabledByDefault' -value 0 -PropertyType 'DWord' -Force | Out-Null
-	New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client' -Force | Out-Null
-	New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client' -name 'Enabled' -value '1' -PropertyType 'DWord' -Force | Out-Null
-	New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client' -name 'DisabledByDefault' -value 0 -PropertyType 'DWord' -Force | Out-Null
-        Write-Host "TLSv1.1 Enabled"
+      New-Item "$base_path\TLS 1.1\Server" -Force | Out-Null
+      New-ItemProperty -path "$base_path\TLS 1.1\Server" -name 'Enabled' -value '1' -PropertyType 'DWord' -Force | Out-Null
+      New-ItemProperty -path "$base_path\TLS 1.1\Server" -name 'DisabledByDefault' -value 0 -PropertyType 'DWord' -Force | Out-Null
+      New-Item "$base_path\TLS 1.1\Client" -Force | Out-Null
+      New-ItemProperty -path "$base_path\TLS 1.1\Client" -name 'Enabled' -value '1' -PropertyType 'DWord' -Force | Out-Null
+      New-ItemProperty -path "$base_path\TLS 1.1\Client" -name 'DisabledByDefault' -value 0 -PropertyType 'DWord' -Force | Out-Null
+      Write-Host "TLSv1.1 Enabled" -ForegroundColor $fg_color
     }
-    else{
-        New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server' -Force | Out-Null
-	New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server' -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
-	New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Server' -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force | Out-Null
-	New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client' -Force | Out-Null
-	New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client' -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
-	New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.1\Client' -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force | Out-Null
-        Write-Host "TLSv1.1 Disabled"
+    else {
+      New-Item "$base_path\TLS 1.1\Server" -Force | Out-Null
+      New-ItemProperty -path "$base_path\TLS 1.1\Server" -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
+      New-ItemProperty -path "$base_path\TLS 1.1\Server" -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force | Out-Null
+      New-Item "$base_path\TLS 1.1\Client" -Force | Out-Null
+      New-ItemProperty -path "$base_path\TLS 1.1\Client" -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
+      New-ItemProperty -path "$base_path\TLS 1.1\Client" -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force | Out-Null
+      Write-Host "TLSv1.1 Disabled" -ForegroundColor $fg_color
     }
+  }
+  catch {
+    Write-Host $_.Exception.Message -ForegroundColor RED
+  }
+  finally {
+    $Error.Clear()
+  }
 }
 
 function tls12 {
+  try {
     if($tls12){
-        New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Server' -Force | Out-Null
-	New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Server' -name 'Enabled' -value '1' -PropertyType 'DWord' -Force | Out-Null
-	New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Server' -name 'DisabledByDefault' -value 0 -PropertyType 'DWord' -Force | Out-Null
-	New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client' -Force | Out-Null
-	New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client' -name 'Enabled' -value '1' -PropertyType 'DWord' -Force | Out-Null
-	New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client' -name 'DisabledByDefault' -value 0 -PropertyType 'DWord' -Force | Out-Null
-        Write-Host "TLSv1.2 Enabled"
+      New-Item "$base_path\TLS 1.2\Server" -Force | Out-Null
+      New-ItemProperty -path "$base_path\TLS 1.2\Server" -name 'Enabled' -value '1' -PropertyType 'DWord' -Force | Out-Null
+      New-ItemProperty -path "$base_path\TLS 1.2\Server" -name 'DisabledByDefault' -value 0 -PropertyType 'DWord' -Force | Out-Null
+      New-Item "$base_path\TLS 1.2\Client" -Force | Out-Null
+      New-ItemProperty -path "$base_path\TLS 1.2\Client" -name 'Enabled' -value '1' -PropertyType 'DWord' -Force | Out-Null
+      New-ItemProperty -path "$base_path\TLS 1.2\Client" -name 'DisabledByDefault' -value 0 -PropertyType 'DWord' -Force | Out-Null
+      Write-Host "TLSv1.2 Enabled" -ForegroundColor $fg_color
     }
-    else{
-        New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Server' -Force | Out-Null
-	New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Server' -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
-	New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Server' -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force | Out-Null
-	New-Item 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client' -Force | Out-Null
-	New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client' -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
-	New-ItemProperty -path 'HKLM:\SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\TLS 1.2\Client' -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force | Out-Null
-        Write-Host "TLSv1.2 Disabled"
+    else {
+      New-Item "$base_path\TLS 1.2\Server" -Force | Out-Null
+      New-ItemProperty -path "$base_path\TLS 1.2\Server" -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
+      New-ItemProperty -path "$base_path\TLS 1.2\Server" -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force | Out-Null
+      New-Item "$base_path\TLS 1.2\Client" -Force | Out-Null
+      New-ItemProperty -path "$base_path\TLS 1.2\Client" -name 'Enabled' -value '0' -PropertyType 'DWord' -Force | Out-Null
+      New-ItemProperty -path "$base_path\TLS 1.2\Client" -name 'DisabledByDefault' -value 1 -PropertyType 'DWord' -Force | Out-Null
+      Write-Host "TLSv1.2 Disabled" -ForegroundColor $fg_color
     }
+  }
+  catch {
+    Write-Host $_.Exception.Message -ForegroundColor RED
+  }
+  finally {
+    $Error.Clear()
+  }
 }
 
 <# TO BE IMPLEMENTED
 function tls13 {
+  try {
     if($tls13){
         Write-Host "TLSv1.3 Enabled"
     }
-    else{
+    else {
         Write-Host "TLSv1.3 Disabled"
     }
+  }
+  catch {
+    Write-Host $_.Exception.Message -ForegroundColor RED
+  }
+  finally {
+    $Error.Clear()
+  }
 }
 #>
 
